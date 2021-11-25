c3251ec376258fbc908983a263304783fa34a644
Alessandro <luisialessandro95@gmail.com>
2021-11-26 00:15:34 +0100

    bonus

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,14 @@
-alert('Ciaone');
+// alert('Ciaone');
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+const app = new Vue({
+    el: '#root',
+    data:{
+        message: "Parigi a primavera",
+        image: "img/parigi_metro.JPG"
+    }
+});
